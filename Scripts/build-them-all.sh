1577417f0255c38d5601271327000e7431639a3f
Maurizio D'Addona <mauritiusdadd@gmail.com>
2015-03-30 11:57:12 +0200

    Updates buildbot building script

--- Scripts/build-them-all.sh
+++ Scripts/build-them-all.sh
@@ -5,13 +5,37 @@ if [[ "$(whoami)" != "buildbot" ]]; then
   exit 1
 fi
 
+SUMMARY="${HOME}/buildbot-build.log"
+
+[[ -e "$SUMMARY" ]] && rm $SUMMARY
+
 export PKGDIR="/home/buildbot/aur"
+export LOGONLY="yes"
+export LC_ALL=C
 
-echo "Building packages from '${PKGDIR}' as '$(whoami)'"
+logmsg()
+{
+  echo "$@" | tee -a "$SUMMARY"
+}
 
-cd "${HOME}" && git pull
-cd "${PKGDIR}" && git pull
+logmsg "buildbot - $(date -u) - building summary"
+logmsg "Building packages from '${PKGDIR}'"
+
+logmsg "Updating dot-files from git repo"
+cd "${HOME}" && git pull oring master &> /dev/null 
+[[ "$?" -eq 0  ]] || echo "Warning: cannot update dot-files"
+
+logmsg "Updating AUR packages from git repo"
+cd "${PKGDIR}" && git pull orign master &> /dev/null 
+[[ "$?" -eq 0  ]] || echo "Warning: cannot update AUR packages"
 
 "${PKGDIR}/ccm_test.sh" all 64
 "${PKGDIR}/ccm_test.sh" all 32
+
+logmsg "--- x86_64 ---"
+grep "\[STATUS\]" "${PKGDIR}/ccm64.log" >> "$SUMMARY"
+logmsg "--- i686 ---"
+grep "\[STATUS\]" "${PKGDIR}/ccm32.log" >> "$SUMMARY"
+
+logmsg "buildbot - $(date -u) - building ends"
 
