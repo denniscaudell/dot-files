d650dfaac9dfb7a9bf41769ec2bb1f07260fa0ec
D'Addona Maurizio <daddona@localhost.localdomain>
2015-03-30 18:23:27 +0200

    Adds notifications to buildot building script

--- Scripts/build-them-all.sh
+++ Scripts/build-them-all.sh
@@ -6,6 +6,7 @@ if [[ "$(whoami)" != "buildbot" ]]; then
 fi
 
 SUMMARY="${HOME}/buildbot-build.log"
+NOTIFY_HEADER="BuildBot sent you a message"
 
 [[ -e "$SUMMARY" ]] && rm $SUMMARY
 
@@ -13,21 +14,44 @@ export PKGDIR="/home/buildbot/aur"
 export LOGONLY="yes"
 export LC_ALL=C
 
+notifyall()
+{
+  for user in "$(who | awk '{print $1}')"; do
+    sudo -u "$user" notify-send -u normal "$NOTIFY_HEADER" "$*"
+  done
+
+  wall "$*"
+}
+
+
 logmsg()
 {
   echo "$@" | tee -a "$SUMMARY"
 }
 
+notifyall "I'm starting the weekly building check for the AUR packages."\
+          "Please, dont't turn off the PC... It won't take too long ;)"
+
 logmsg "buildbot - $(date -u) - building summary"
 logmsg "Building packages from '${PKGDIR}'"
 
 logmsg "Updating dot-files from git repo"
-cd "${HOME}" && timeout 10 git pull origin master &> /dev/null 
-[[ "$?" -eq 0  ]] || echo "Warning: cannot update dot-files"
+cd "${HOME}" && timeout 10 git pull origin master &> /dev/null
+
+if [[ "$?" -ne 0  ]]; then
+  echo "Warning: cannot update dot-files"
+  notifyall "Something went wrong when I tried to"\
+            "update the git repo 'dot-files'"
+fi
 
 logmsg "Updating AUR packages from git repo"
-cd "${PKGDIR}" && timeout 10 git pull origin master &> /dev/null 
-[[ "$?" -eq 0  ]] || echo "Warning: cannot update AUR packages"
+cd "${PKGDIR}" && timeout 10 git pull origin master &> /dev/null
+
+if [[ "$?" -ne 0  ]]; then
+  echo "Warning: cannot update AUR packages"
+  notifyall "Something went wrong when I tried to"\
+            "update the AUR packages from the git repo"
+fi
 
 "${PKGDIR}/ccm_test.sh" all 64
 "${PKGDIR}/ccm_test.sh" all 32
@@ -38,4 +62,9 @@ logmsg "--- i686 ---"
 grep "\[STATUS\]" "${PKGDIR}/ccm32.log" >> "$SUMMARY"
 
 logmsg "buildbot - $(date -u) - building ends"
+
+notifyall "Ok, I finished the building. See you the next time ;)\n"\
+          "\n Here there is the building summary:\n"\
+          "\n$(egrep '\[STATUS\].*(succeded|failed)' ${SUMMARY} |\
+           awk '{printf "%-20s %s\n", $3, $8}')"
 
