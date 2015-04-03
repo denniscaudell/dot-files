399018ab9e72600bab12a38ee065849773ba6818
Maurizio D'Addona <mauritiusdadd@gmail.com>
2015-04-03 15:18:24 +0200

    Adding shutdown procedure to buildbot building script

--- Scripts/build-them-all.sh
+++ Scripts/build-them-all.sh
@@ -66,3 +66,10 @@ notifyall "Ok, I finished the building. See you the next time ;)\n"\
           "\n$(egrep '\[STATUS\].*(succeded|failed)' ${SUMMARY} |\
            awk '{printf "%-20s %s\n", $3, $8}')"
 
+if [[ "$(who | wc -l)" -eq 0]]; then
+  # well, there is nobody logged-in :(
+  # let's shutdown the machine then ;)
+  logmsg "No user connected, shutting down..."
+  shutdown now
+fi
+
