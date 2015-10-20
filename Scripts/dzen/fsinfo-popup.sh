59791b841b174201fd4bf4869ac1caa9b4974a24
Maurizio D'Addona <mauritiusdadd@gmail.com>
2015-10-20 22:51:15 +0200

    Update controls for new laptop: macbook-pro early 2015

--- Scripts/dzen/fsinfo-popup.sh
+++ Scripts/dzen/fsinfo-popup.sh
@@ -14,6 +14,6 @@
     }' | sort
 ) | dzen2 -p -x "350" -y "-30" -w "350" -l "5" -sa 'l' -ta 'c'\
    -title-name 'popup_sysinfo' -e 'onstart=uncollapse;button1=exit;button3=exit'\
-   -bg \#000000 -fn "DejaVu Sans Mono for Powerline:size=8"
+   -bg \#000000 -fn "DejaVu Sans Mono for Powerline:size=12"
 
 # "onstart=uncollapse" ensures that slave window is visible from start.
