ee5a0afe7b96b970ff95d1eeb13ae7f7ff7629ea
Maurizio D'Addona <mauritiusdadd@gmail.com>
2017-11-29 20:08:22 +0100

    Update sway script

--- Scripts/startsway.sh
+++ Scripts/startsway.sh
@@ -9,4 +9,10 @@ export QT_QPA_PLATFORM="xcb"
 export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
 export _JAVA_AWT_WM_NONREPARENTING=1
 
-sway
+if command -v sway; then
+  sway
+elif command -v i3; then
+  i3
+else
+  echo "No compatible window manager found"
+fi
